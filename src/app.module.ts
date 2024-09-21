3b47c5539b1853f6964e99acf651f83236bb79f6
Fabricio Araujo <fabricioaraujo051@gmail.com>
2024-09-21 13:54:50 -0300

    feat: adicionando conexão com mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './modules/todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:***admin@cluster0.mr7en.mongodb.net/tarefas?retryWrites=true&w=majority&appName=Cluster0'
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     TodoModule,
   ],
 })
